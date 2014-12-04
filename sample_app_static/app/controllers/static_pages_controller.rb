e9874765306e53bfbee682cb1f3bd108e7f2fef0
Dipak Pawar <dipak@vertisinfotech.com>
2014-12-04 09:29:03 +0530

    static pages completed

--- sample_app_static/app/controllers/static_pages_controller.rb
+++ sample_app_static/app/controllers/static_pages_controller.rb
@@ -1,7 +1,12 @@
 class StaticPagesController < ApplicationController
   def home
+    #render text: "Hello Home page"
   end
 
   def help
+  end
+
+
+  def about
   end
 end
--- sample_app_static/config/routes.rb
+++ sample_app_static/config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  # this line calls the static page controller and home action in that.
+  root 'static_pages#home'
+
+  #get 'static_pages/home'
 
   get 'static_pages/help'
+
+  get 'static_pages/about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
