89f3b5eb68fbd5c6a33b0f65b4c889fae2233901
Sunil Kotwal <kotwalsa@gmail.com>
2014-12-06 18:58:11 +0530

    Now added header footer along with partial layout to separate each section instead of all combines in aplication layoout

--- sample_app_static/app/helpers/application_helper.rb
+++ sample_app_static/app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def full_title title = ''
+    base_title = 'Ruby on Rails Tutorial Sample App'
+    if title.empty?
+      full_title = base_title
+    else
+      full_title "#{title} | #{base_title}"
+    end
+  end
+
 end
