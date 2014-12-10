600924b63e3755f6ee6c9a65856e7b267739dcee
Sunil Kotwal <kotwalsa@gmail.com>
2014-12-11 00:36:06 +0530

    till middle of show action of images remianing addind css part to same

--- sample_app_static/app/controllers/users_controller.rb
+++ sample_app_static/app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def show
+  @user = User.find(params[:id])
+# Below line is used for debugger purpose
+  #debugger
+  end
+
 end
