11eef114dc6faba4847f2e52c8d0d091d740868e
Sunil Kotwal <kotwalsa@gmail.com>
2014-12-11 15:32:51 +0530

    Users signin with validation and error messages gets complete

--- sample_app_static/app/controllers/users_controller.rb
+++ sample_app_static/app/controllers/users_controller.rb
@@ -1,11 +1,34 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def show
   @user = User.find(params[:id])
-# Below line is used for debugger purpose
+  # Below line is used for debugger purpose using byebug gem
   #debugger
   end
 
+  def index
+
+  end
+
+  def create
+    @user = User.new(users_params)
+    if @user.save
+      flash[:sucess] = "Welcome to the sample app"
+      redirect_to @user
+    else
+      #logger.debug "="*80
+      #logger.debug "In else"
+      #flash[:error] = "Something gets wrong"
+      #logger.debug "rendering new"
+      render 'new'
+    end
+  end
+
+  private
+   def users_params
+    params.require(:user).permit(:name,:email,:password)
+   end
 end
--- sample_app_static/app/models/user.rb
+++ sample_app_static/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   #attr_accessible :name, :email, :password, :password_confirmation
-  validates :name, presence: true, length: { maximum: 50 }
+  validates :name, presence: true, length: { maximum: 20 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
               uniqueness: { case_sensitive: false }
