02037e98e69088e2e767dd052eaec26b140e3ec2
Dipak Pawar <dipakpawar231@gmail.com>
2014-12-21 22:27:51 +0530

    removing unwanted migrations

--- sample_app_static/db/migrate/20141213012008_add_remember_token_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddRememberTokenToUsers < ActiveRecord::Migration
-  def change
-    #rails g migration add_remember_token_to_users remember_token:string
-    add_column :users, :remember_token, :string
-  end
-end
